058fe7f37111018b07de17c02e67de863072dfb8
jerfsonsilva <jerfsonlink@gmail.com>
2022-01-12 22:20:37 -0300

    Task-04 - Create routes

---
+++ app/Http/Controllers/ImportUserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImportUserController extends Controller
+{
+ public function index($tipo, Request $request)
+ {
+     return 'created';
+ }
+}
