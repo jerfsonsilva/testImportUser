d5e4be9b07cd0fcc4a79e59001ae4c7bab8f5d67
jerfsonsilva <jerfsonlink@gmail.com>
2022-01-15 09:37:35 -0300

    Task-07 - Create function verifyUserExist

--- app/Models/people.php
+++ app/Models/people.php
@@ -11,10 +11,10 @@ class people extends Model
 
     public static function createPeople($people)
     {
-        $fields = ['name', 'address', 'checked', 'description', 'interest', 'email', 'account'];
+        $fields = ['name', 'account', 'checked', 'description', 'interest', 'email', 'account'];
         $newPeople = new people;
-        //TODO verify if it exists 
-        //verifyUserExist($people)
+
+        if(people::verifyUserExist($people) > 0) return false;
         //verifyAgeUser($user)
         foreach ($fields as $key => $field) {
             $newPeople[$field] = isset($people[$field]) ? $people[$field] : null;
@@ -22,15 +22,17 @@ class people extends Model
         $newPeople['date_of_birth'] = isset($people['date_of_birth']) ? date("Y-m-d", strtotime($people['date_of_birth'])) : null;
         if ($newPeople->save()) {
             if ($people['credit_card'])
-                \App\Models\people_credit_card::createCard($people['credit_card'],$newPeople->id);
+                \App\Models\people_credit_card::createCard($people['credit_card'], $newPeople->id);
+            return true;
         }
+        return false;
     }
     public static function verifyUserExist($people)
     {
-        //TODO validation if it exists in the database name and adress, and credit_cart
+        return people::where('email', $people['email'])->where('account', $people['account'])->count();
     }
-    public static function verifyAgeUser($user)
+    public static function verifyAgeUser($people)
     {
-       //TODO validation age of records between 18 and 65 (or unknown).
+        //TODO validation age of records between 18 and 65 (or unknown).
     }
 }
